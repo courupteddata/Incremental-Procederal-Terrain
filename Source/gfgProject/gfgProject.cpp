d09a61dd1db219e8314127e0895b589dcffcd04a
Nathan <nathan@jones.one>
2017-12-04 17:00:05 -0500

    Initial commit
    The way it is layed out:
    Attempt1 - using material
    Attempt2 - using random float and no normals
    Attempt3 - using random float and normals
    Attempt4 - Simple noise and normals
    Attempt5 - Complex noise and normals. Also tuned for displying.
    Attempt5_no_normals - Complex noise and no normals. Clone of attempt 5 without the normals.

---
+++ Source/gfgProject/gfgProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "gfgProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, gfgProject, "gfgProject" );
